9fb401e1a2bbb0c764f5986d5f0749520ef8345a
africabusinessnews <71972487+africabusinessnews@users.noreply.github.com>
2021-09-22 20:57:34 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,14 +23,7 @@ function App() {
     <>
     <Helmet>
     <title>Deep Space Explorer</title>
-      <meta name="Hilarious Huskies is an NFT collection inspired a beautiful pup named Hazel." content="Hilarious Huskies is an NFT collection inspired a beautiful pup named Hazel." />
-      <meta content="summary_large_image" property="twitter:card" />
-      <meta content="HilariousHuskies Marketplace: Mint and explore digital assets" property="og:title" />
-      <meta content="Hilarious Huskies is an NFT collection inspired by a beautiful Siberian Husky, named Hazel. Loyal, friendly, and plagued with wanderlust, this collection is a tribute to our furry friends, doing things you would normally find huskies doing." property="og:description" />
-      <meta content={banner} property="og:image" />
-      <link href="https://hilarioushuskies.life" rel="canonical" />
-      <meta content="Hilarious Huskies is an NFT collection inspired by a beautiful Siberian Husky, named Hazel. Loyal, friendly, and plagued with wanderlust, this collection is a tribute to our furry friends, doing things you would normally find huskies doing." name="description" />
-    </Helmet>
+       </Helmet>
     <Wallet />
    
       </>
